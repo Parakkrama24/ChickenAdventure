753360941a5a34981b0da764aaef02e0a3a2ebc0
Parakkrama24 <110714055+Parakkrama24@users.noreply.github.com>
2022-12-04 01:28:58 +0530

    2nd

---
+++ Assets/Chicken Adventure Assets by CSGD/scripts/chicunScripts/PlayerMovements.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    [SerializeField] private float movespeed = 3.5f;
+    [SerializeField] private Rigidbody2D Rigidbody;
+
+    [SerializeField]private bool _isGoindLeft;
+    private SpriteRenderer _spriteRenderer;
+   
+    void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();    //getcompennets
+        _spriteRenderer = GetComponent<SpriteRenderer>();//getCompent
+    }
+
+    
+    private void Update()
+    {
+        if (_isGoindLeft)//goleft
+        {
+            Rigidbody.velocity = new Vector3(-movespeed, Rigidbody.velocity.y);
+        }
+        else//goright
+        {
+            Rigidbody.velocity = new Vector3(movespeed,Rigidbody.velocity.y);
+        }
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag== "boader")
+        {
+            _isGoindLeft = !_isGoindLeft;
+            _spriteRenderer.flipX = _isGoindLeft;//flipx
+        }
+    }
+   
+}//class

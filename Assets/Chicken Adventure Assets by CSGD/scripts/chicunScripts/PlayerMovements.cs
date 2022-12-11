432400153552b9e6e576448b156f0d731532a214
Parakkrama24 <110714055+Parakkrama24@users.noreply.github.com>
2022-12-11 22:39:13 +0530

    11/12/2022

--- Assets/Chicken Adventure Assets by CSGD/scripts/chicunScripts/PlayerMovements.cs
+++ Assets/Chicken Adventure Assets by CSGD/scripts/chicunScripts/PlayerMovements.cs
@@ -7,15 +7,49 @@ public class PlayerMovements : MonoBehaviour
 
     [SerializeField]private bool _isGoindLeft;
     private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+
+    private float _firstJumpforce = 5f, _secondJumpForce = 7f;
+    private bool _isFirdtJump, _isSecondJump;//false
    
     void Awake()
     {
         Rigidbody = GetComponent<Rigidbody2D>();    //getcompennets
         _spriteRenderer = GetComponent<SpriteRenderer>();//getCompent
+        _animator = GetComponent<Animator>();   //getCompnet                                
     }
-
+    private void Start()
+    {
+       _isFirdtJump = true;
+        _isSecondJump = true;
+    }
+   
     
     private void Update()
+    {
+        movemet();
+        if (Input.GetMouseButtonDown(0))
+        {
+            jumpchicken();
+        }
+    }
+    private void jumpchicken()
+    {
+        if (_isFirdtJump == true)
+        {
+            _isFirdtJump = false;
+            Rigidbody.velocity = new Vector2(Rigidbody.velocity.x, _firstJumpforce);
+            _animator.SetTrigger("Fly");
+        }
+        else if (_isSecondJump == true)
+        {
+            _isSecondJump = false;
+            Rigidbody.velocity = new Vector2(Rigidbody.velocity.x, _secondJumpForce);
+            _animator.SetTrigger("Fly");
+        }
+    }
+
+    private void movemet()
     {
         if (_isGoindLeft)//goleft
         {
@@ -23,16 +57,21 @@ public class PlayerMovements : MonoBehaviour
         }
         else//goright
         {
-            Rigidbody.velocity = new Vector3(movespeed,Rigidbody.velocity.y);
+            Rigidbody.velocity = new Vector3(movespeed, Rigidbody.velocity.y);
         }
-        
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag== "boader")
         {
             _isGoindLeft = !_isGoindLeft;
             _spriteRenderer.flipX = _isGoindLeft;//flipx
+        }
+        if (collision.gameObject.tag == "Graound")
+        {
+            _isFirdtJump = true;
+            _isSecondJump = true;
         }
     }
    
